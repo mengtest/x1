803dd54f790a481605c5a010517433ce36de3043
liaowuping <348984539@qq.com>
2017-06-23 19:21:02 +0800

    去除无用代码

--- Assets/Scripts/Game/Command/Gen/GGenImageCommand.cs
+++ Assets/Scripts/Game/Command/Gen/GGenImageCommand.cs
@@ -31,12 +31,7 @@ namespace berry
             go.transform.localScale = Vector3.one;
 
             Image imgCtrl = go.AddComponent<Image> ();
-#if false
-            Texture2D tex = FResManager.getInstance ().getRes (FResID.TEXTURE) as Texture2D;
-            imgCtrl.sprite = Sprite.Create (tex, new Rect (0, 0, tex.width, tex.height), new Vector2 (0.5f, 0.5f));
-#else
             imgCtrl.sprite = FResManager.getInstance ().getRes (FResID.SPRITE) as Sprite;
-#endif
             imgCtrl.SetNativeSize ();
         }
 
