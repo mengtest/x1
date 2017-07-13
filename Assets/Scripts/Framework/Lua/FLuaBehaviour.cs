a19c85203de3600ed2cf4effd5ace1b8023c1f44
liaowuping <348984539@qq.com>
2017-07-13 19:22:59 +0800

    变量名修改

--- Assets/Scripts/Framework/Lua/FLuaBehaviour.cs
+++ Assets/Scripts/Framework/Lua/FLuaBehaviour.cs
@@ -32,33 +32,34 @@ namespace x1.Framework
             m_luaManager = FLuaManager.getInstance ();
             LuaEnv luaEnv = m_luaManager.getEnv ();
 
-            LuaTable viewScript = luaEnv.Global.Get<LuaTable> (m_className);
-            LuaTable ctrlScript = luaEnv.Global.Get<LuaTable> (m_className + "Ctrl");
+            LuaTable viewTable = luaEnv.Global.Get<LuaTable> (m_className);
+            LuaTable ctrlTable = luaEnv.Global.Get<LuaTable> (m_className + "Ctrl");
 
-            if (viewScript == null) {
+            if (viewTable == null) {
                 Debug.Log ("not found " + m_className + " with in lua scripts");
                 return;
             }
-            if (ctrlScript == null) {
+            if (ctrlTable == null) {
                 Debug.Log ("not found " + m_className + "Ctrl with in lua scripts");
                 return;
             }
 
-            viewScript.Set ("gameObject", gameObject);
-            viewScript.Set ("transform", transform);
+            viewTable.Set ("gameObject", gameObject);
+            viewTable.Set ("transform", transform);
 
-            m_luaInit = viewScript.Get<Action> ("init");
-            m_luaAwake = viewScript.Get<Action> ("Awake");
-            m_luaOnEnable = viewScript.Get<Action> ("OnEnable");
-            m_luaOnDisable = viewScript.Get<Action> ("OnDisable");
-            m_luaStart = viewScript.Get<Action> ("Start");
-            m_luaUpdate = viewScript.Get<Action> ("Update");
-            m_luaFixedUpdate = viewScript.Get<Action> ("FixedUpdate");
-            m_luaOnDestroy = viewScript.Get<Action> ("OnDestroy");
-            m_luaOnApplicationPause = viewScript.Get<Action> ("OnApplicationPause");
-            m_luaOnApplicationFocus = viewScript.Get<Action> ("OnApplicationFocus");
-            m_luaOnApplicationQuit = viewScript.Get<Action> ("OnApplicationQuit");
+            m_luaInit = viewTable.Get<Action> ("init");
+            m_luaAwake = viewTable.Get<Action> ("Awake");
+            m_luaOnEnable = viewTable.Get<Action> ("OnEnable");
+            m_luaOnDisable = viewTable.Get<Action> ("OnDisable");
+            m_luaStart = viewTable.Get<Action> ("Start");
+            m_luaUpdate = viewTable.Get<Action> ("Update");
+            m_luaFixedUpdate = viewTable.Get<Action> ("FixedUpdate");
+            m_luaOnDestroy = viewTable.Get<Action> ("OnDestroy");
+            m_luaOnApplicationPause = viewTable.Get<Action> ("OnApplicationPause");
+            m_luaOnApplicationFocus = viewTable.Get<Action> ("OnApplicationFocus");
+            m_luaOnApplicationQuit = viewTable.Get<Action> ("OnApplicationQuit");
 
+            // 注册按钮的回调函数
             var btns = transform.GetComponentsInChildren<Button> (true);
             foreach (var btn in btns) {
                 FLuaButtonEvent buttnEvent = btn.GetComponent<FLuaButtonEvent> ();
@@ -68,7 +69,7 @@ namespace x1.Framework
 
             if (m_luaInit != null)
                 m_luaInit ();
-            
+
             if (m_luaAwake != null)
                 m_luaAwake ();
         }
@@ -85,7 +86,6 @@ namespace x1.Framework
                 m_luaOnDisable ();
         }
 
-        // Use this for initialization
         void Start ()
         {
             if (m_luaStart != null) {
@@ -93,7 +93,6 @@ namespace x1.Framework
             }
         }
 
-        // Update is called once per frame
         void Update ()
         {
             if (m_luaUpdate != null)
@@ -110,11 +109,6 @@ namespace x1.Framework
         {
             if (m_luaOnDestroy != null)
                 m_luaOnDestroy ();
-
-            m_luaAwake = null;
-            m_luaOnDestroy = null;
-            m_luaUpdate = null;
-            m_luaStart = null;
         }
 
         void OnApplicationPause ()
