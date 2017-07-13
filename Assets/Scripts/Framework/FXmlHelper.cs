b8debc8429db220aafd7f1500f2404737d053421
liaowuping <348984539@qq.com>
2017-07-13 18:15:37 +0800

    Init 场景修改上传

---
+++ Assets/Scripts/Framework/FXmlHelper.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System;
+using System.Security;
+using System.Collections;
+using Mono.Xml;
+
+namespace x1.Framework
+{
+    public static class FXmlHelper
+    {
+        //        public static SecurityElement loadXMLWithFile ()
+        //        {
+        //        }
+        //
+        public static SecurityElement loadXML (string content)
+        {
+            try {
+                SecurityParser sp = new SecurityParser ();
+                sp.LoadXml (content);
+                return sp.ToXml ();
+            } catch (Exception ex) {
+                Debug.LogError (ex);
+                return null;
+            }
+        }
+
+
+    }
+}
